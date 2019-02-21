6c2bfe75f3c647df603f619369213c6696a61c66
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-02-21 18:07:32 +0200

    add missing import

--- core/network/src/service.rs
+++ core/network/src/service.rs
@@ -17,7 +17,7 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::{io, thread};
-use log::{warn, debug, error, trace};
+use log::{warn, debug, error, trace, info};
 use futures::{Async, Future, Stream, stream, sync::oneshot};
 use parking_lot::Mutex;
 use network_libp2p::{ProtocolId, PeerId, NetworkConfiguration, NodeIndex, ErrorKind, Severity};
